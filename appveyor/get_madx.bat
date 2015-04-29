94442b551fca2afc90ba73f3cabbaf6e42eba936
Thomas Gläßle <t_glaessle@gmx.de>
2015-04-29 06:14:35 +0200

    Remove run_with_compiler.cmd
    As we don't need it anymore:
    - python dependencies are downloaded as .whl from the web
    - actual cpymad package is built with mingw-static

--- appveyor/get_madx.bat
+++ appveyor/get_madx.bat
@@ -1,12 +1,12 @@
 setlocal
 
 set madx_repo=http://pypi.coldfix.de/libmadx-dev
-set madx_archive=madx-win%PYTHON_ARCH%.7z
+set madx_archive=madx-win%ARCH%.7z
 set madx_download=%madx_repo%/%madx_archive%
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile($env:madx_download, $env:madx_archive)"
 
 7z x %madx_archive%
-move madx-win%PYTHON_ARCH% %1
+move madx-win%ARCH% %1
 
 endlocal
